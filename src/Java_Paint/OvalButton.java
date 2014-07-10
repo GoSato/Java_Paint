6df62058f6c3fff967ffb314a4ab1e1d7bacfc16
GoSato <GoSato@satou-no-mbp.lan>
2014-07-11 01:51:15 +0900

    フォルダ名を修正

---
+++ src/Java_Paint/OvalButton.java
@@ -0,0 +1,54 @@
+package Java_Paint;
+
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+public class OvalButton extends JButton{
+	StateManager stateManager;
+	
+	public OvalButton(StateManager stateManager){
+		super("Oval");
+		
+		addActionListener(new OvalListener());
+		
+		this.stateManager = stateManager;
+	}
+	
+	class OvalListener implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			stateManager.setState(new OvalState(stateManager));
+			stateManager.mediator.resetSelectd();
+			stateManager.mediator.canvas.repaint();
+		}
+	}
+	
+	public static class OvalState extends State{
+		int x1, y1;
+		MyOval oval;
+		StateManager stateManager;
+		
+		public OvalState(StateManager stateManager){
+			this.stateManager = stateManager;
+		}
+		
+		public void mouseDown(int x, int y){
+			oval = new MyOval(x, y, 0, 0);
+			x1 = x;
+			y1 = y;
+			
+			oval.setLinePattern(stateManager.linePattern);
+			stateManager.addDrawing(oval);
+		}
+		
+		public void mouseUp(int x, int y){
+			oval.setRegion();
+		}
+		
+		public void mouseDrag(int x, int y){
+			oval.setSize(x-x1, y-y1);
+		}
+	}
+
+}
---
+++ src/Java_Paint/State.java
@@ -0,0 +1,7 @@
+package Java_Paint;
+
+public class State {
+	public void mouseDown(int x, int y){}
+	public void mouseUp(int x, int y){}
+	public void mouseDrag(int x, int y){}
+}
